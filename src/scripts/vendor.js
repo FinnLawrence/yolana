ae124f16609ed96909123a075b1fede0854021bb
Finn Lawrence <finn.lawrence@gmail.com>
2018-05-09 21:53:08 +1200

    Fixed twitter feed

--- src/scripts/vendor.js
+++ src/scripts/vendor.js
@@ -9,7 +9,6 @@
 // =require vendor/jquery-2.2.3.min.js
 
 // =require vendor/mc-validate.js
-// =require vendor/twitter-widget.js
 
 // Attempts to preserve comments that likely contain licensing information,
 // even if the comment does not have directives such as `@license` or `/*!`.
